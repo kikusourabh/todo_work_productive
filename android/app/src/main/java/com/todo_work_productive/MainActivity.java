f8a3b82fe3d610facb5e8bbcb5a24dfd683b339d
kikusourabh <karmakarsourabh7@gmail.com>
2020-08-31 15:34:06 +0530

    inital commit

---
+++ android/app/src/main/java/com/todo_work_productive/MainActivity.java
@@ -0,0 +1,15 @@
+package com.todo_work_productive;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "Todo_work_productive";
+  }
+}
